d62a2537122e62a85740789c316b903dc3b503ac
Abdulrahmankhaled1111 <abd.ulrahman.khaled@mni.thm.de>
2026-06-25 11:19:06 +0200

    Logo und Wartungstext aktualisieren

--- assets/site-status.js
+++ assets/site-status.js
@@ -2,5 +2,5 @@ window.HAMZA_SITE_STATUS = {
   maintenance: false,
   title: "Wir arbeiten gerade an der Seite.",
   message:
-    "Die Website wird aktuell ueberarbeitet. Fuer Besichtigungen und Auftragsabstimmungen sind wir telefonisch unter +49 1590 1328051 oder per E-Mail an hamzabau06@gmail.com erreichbar."
+    "Die Website wird aktuell überarbeitet. Für Besichtigungen und Auftragsabstimmungen sind wir telefonisch unter +49 1590 1328051 oder per E-Mail an hamzabau06@gmail.com erreichbar."
 };
